35639c1177f9cca4d1fa929d9375b81a6cd8fbbf
ylimataba <matsanger@hotmail.com>
2014-12-04 16:12:38 +0200

    Update projekti.ino

--- projekti.ino
+++ projekti.ino
@@ -2,6 +2,7 @@
 #include <SPI.h>
 #include <PN532_SPI.h>
 #include "PN532.h"
+#include <NfcAdapter.h>
 #include <LiquidCrystal.h>
 
 Sd2Card card;
@@ -10,13 +11,13 @@ SdFile root;
 File myFile;
 
 PN532_SPI pn532spi(SPI, 10);
-PN532 nfc(pn532spi);
+NfcAdapter nfc = NfcAdapter(pn532spi);
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 int COL = 0;
 int ROW = 0;
 
-char* STRING;
+char* STRING = (char*)malloc(sizeof(char)*32);
 int STRINGSIZE = 0;
 int BUTTON[12][3] = {{1, 770, 845},      // button 1
                      {2, 440, 470},      // button 2
@@ -34,7 +35,7 @@ int ANALOGPIN = A5;       // analog pin to read the buttons
 int LABEL = 0;            // for reporting the button label
 int COUNTER = 0;          // how many times we have seen new value
 long TIME = 0;            // the last time the output pin was sampled
-int DEBOUNCE_COUNT = 150; // number of millis/samples to consider before declaring a debounced input
+int DEBOUNCE_COUNT = 30; // number of millis/samples to consider before declaring a debounced input
 int CURRENT_STATE = 0;    // the debounced input value
 int BUTTONVAL;
 boolean dot = false;
@@ -56,10 +57,12 @@ void setup(void)
 
 void loop(void)
 {
+  lcd.clear();
+  setCursor(0,0);
   printString("Hello!");
   setCursor(0,1);
   printString("Please identify yourself with   a tag");
-
+  
   startNFC();
   char* tag = getTag();    
   
@@ -67,10 +70,31 @@ void loop(void)
   {
     lcd.clear();
     setCursor(0,0);
+    printString(tag);
+    setCursor(0,2);
     printString("Tag accepted!");
-    startSD();
-    testWrite(tag);
+    //startSD();
+    //testWrite(tag);
     delay(1000);
+    
+    lcd.clear();
+    setCursor(0,0);
+    printString("Insert amout:");
+    setCursor(0,1);
+    while(LABEL != 10)
+    {
+      keypad();
+    }
+    LABEL = 0;
+    STRING[STRINGSIZE-1] = '\0';
+    STRINGSIZE = 0;
+    lcd.clear();
+    setCursor(0,3);
+    printString(STRING);
+    
+    setCursor(0,0);
+    printString("Pleasure to     make business   with you!");
+    delay(5000);
   }
 }
 
@@ -101,6 +125,7 @@ void testWrite(char* tag)
     setCursor(0,0);
     printString("Pleasure to     make business   with you!");
   }
+  else
+    printString("Sorry I fucked up!");
 }
-
 
