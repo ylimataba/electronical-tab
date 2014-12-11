c17cbb5b38a717b501a6d0df3b1cb407abdea782
ylimataba <matsanger@hotmail.com>
2014-12-11 13:08:59 +0200

    Update projekti.ino

--- projekti.ino
+++ projekti.ino
@@ -5,11 +5,6 @@
 #include <NfcAdapter.h>
 #include <LiquidCrystal.h>
 
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-File myFile;
-
 PN532_SPI pn532spi(SPI, 10);
 NfcAdapter nfc = NfcAdapter(pn532spi);
 
@@ -17,29 +12,30 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 int COL = 0;
 int ROW = 0;
 
-char* STRING = (char*)malloc(sizeof(char)*32);
+char* STRING = (char*)malloc(sizeof(char)*7);
 int STRINGSIZE = 0;
 int BUTTON[12][3] = {{1, 770, 845},      // button 1
                      {2, 440, 470},      // button 2
                      {3, 101, 145},      // button 3
-                     {4, 690, 750},      // button 4
+                     {4, 710, 750},      // button 4
                      {5, 280, 320},      // button 5
                      {6, 61, 100},       // button 6
-                     {7, 650, 689},      // button 7
+                     {7, 650, 709},      // button 7
                      {8, 191, 220},      // button 8
                      {9, 10, 60},        // button 9
                      {10, 550, 630},     // enter
                      {11, 146, 190},     // button 0
                      {12, 900, 1023}};   // cancel
 int ANALOGPIN = A5;       // analog pin to read the buttons
-int LABEL = 0;            // for reporting the button label
+volatile int LABEL = 0;            // for reporting the button label
 int COUNTER = 0;          // how many times we have seen new value
 long TIME = 0;            // the last time the output pin was sampled
-int DEBOUNCE_COUNT = 30; // number of millis/samples to consider before declaring a debounced input
+int DEBOUNCE_COUNT = 3; // number of millis/samples to consider before declaring a debounced input
 int CURRENT_STATE = 0;    // the debounced input value
 int BUTTONVAL;
 boolean dot = false;
 long TIMER = 0;
+
 
 void setup(void)
 {
@@ -56,47 +52,23 @@ void setup(void)
 }
 
 void loop(void)
-{
+{ 
   lcd.clear();
   setCursor(0,0);
   printString("Hello!");
   setCursor(0,1);
-  printString("Please identify yourself with   a tag");
-  
+  printString("Please identify yourself with   a tag!");
   startNFC();
-  char* tag = getTag();    
+  char* tag = getTag();
   
-  if(tag[0] != '\0')
-  {
-    lcd.clear();
-    setCursor(0,0);
-    printString(tag);
-    setCursor(0,2);
-    printString("Tag accepted!");
-    //startSD();
-    //testWrite(tag);
-    delay(1000);
-    
-    lcd.clear();
-    setCursor(0,0);
-    printString("Insert amout:");
-    setCursor(0,1);
-    while(LABEL != 10)
-    {
-      keypad();
-    }
-    LABEL = 0;
-    STRING[STRINGSIZE-1] = '\0';
-    STRINGSIZE = 0;
-    lcd.clear();
-    setCursor(0,3);
-    printString(STRING);
-    
-    setCursor(0,0);
-    printString("Pleasure to     make business   with you!");
-    delay(5000);
+  if (tag[0] != '\0')
+  { 
+    askInput(tag);
   }
 }
+
+
+
 
 void startSD()
 {
@@ -104,28 +76,3 @@ void startSD()
   digitalWrite(A1,LOW);
   SD.begin(A1);
 }
-
-void testWrite(char* tag)
-{
-  myFile = SD.open("test.txt", FILE_WRITE);
-  if (myFile)
-  {
-    myFile.println(tag);
-    lcd.clear();
-    setCursor(0,0);
-    printString("Insert amout:");
-    setCursor(0,1);
-    while(LABEL != 10)
-    {
-      keypad();
-    }
-    myFile.println(STRING);
-    myFile.close();
-    lcd.clear();
-    setCursor(0,0);
-    printString("Pleasure to     make business   with you!");
-  }
-  else
-    printString("Sorry I fucked up!");
-}
-
