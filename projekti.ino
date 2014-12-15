da48d99475082f9d3a36ebb2b2f79841adb421d1
ylimataba <matsanger@hotmail.com>
2014-12-15 14:40:03 +0200

    Update projekti.ino

--- projekti.ino
+++ projekti.ino
@@ -12,29 +12,26 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 int COL = 0;
 int ROW = 0;
 
-char* STRING = (char*)malloc(sizeof(char)*7);
+char* STRING = (char*)malloc(sizeof(char)*9);
 int STRINGSIZE = 0;
-int BUTTON[12][3] = {{1, 770, 845},      // button 1
-                     {2, 440, 470},      // button 2
-                     {3, 101, 145},      // button 3
-                     {4, 710, 750},      // button 4
-                     {5, 280, 320},      // button 5
-                     {6, 61, 100},       // button 6
-                     {7, 650, 709},      // button 7
-                     {8, 191, 220},      // button 8
-                     {9, 10, 60},        // button 9
-                     {10, 550, 630},     // enter
-                     {11, 146, 190},     // button 0
-                     {12, 900, 1023}};   // cancel
-int ANALOGPIN = A5;       // analog pin to read the buttons
-volatile int LABEL = 0;            // for reporting the button label
+
+char* tag = (char*)malloc(sizeof(char)*15);
+
+int BUTTON[12][3] = {{1, 740, 770},      // button 1
+                     {2, 120, 140},      // button 2
+                     {3, 440, 460},      // button 3
+                     {4, 20, 40},      // button 4
+                     {5, 145, 165},      // button 5
+                     {6, 500, 530},       // button 6
+                     {7, 55, 75},      // button 7
+                     {8, 220, 250},      // button 8
+                     {9, 530, 560},        // button 9
+                     {10, 330, 360},     // button 0
+                     {11, 80, 110},     // enter
+                     {12, 600, 640}};  // cancel
 int COUNTER = 0;          // how many times we have seen new value
-long TIME = 0;            // the last time the output pin was sampled
-int DEBOUNCE_COUNT = 3; // number of millis/samples to consider before declaring a debounced input
+int DEBOUNCE_COUNT = 30;  // number of millis/samples to consider before declaring a debounced input
 int CURRENT_STATE = 0;    // the debounced input value
-int BUTTONVAL;
-boolean dot = false;
-long TIMER = 0;
 
 
 void setup(void)
@@ -49,30 +46,68 @@ void setup(void)
   digitalWrite(7,HIGH);
   analogWrite(8,90);   // Contrast
   lcd.begin(16,4);
+  lcd.clear();
+  setCursor(0,0);
 }
 
 void loop(void)
 { 
-  lcd.clear();
-  setCursor(0,0);
-  printString("Hello!");
-  setCursor(0,1);
-  printString("Please identify yourself with   a tag!");
-  startNFC();
-  char* tag = getTag();
+  if(COL == 0 && ROW == 0)
+  {
+    printString("Hello!");
+    setCursor(0,1);
+    printString("Please identify yourself with   a tag!");
+  }
+
+  tag = getTag();
   
   if (tag[0] != '\0')
-  { 
-    askInput(tag);
+  {
+    lcd.clear();
+    setCursor(0,0);
+    printString("1: ADD");
+    setCursor(0,1);
+    printString("2: REDUCE");
+    setCursor(0,2);
+    printString("3: SHOW TAB INFO");
+    setCursor(0,3);
+    printString("4: QUIT");
+    int label = 0;
+    long tiima = 0;
+    while(1)
+    {
+      if(millis() != tiima)
+      {
+        label = keypad(0,4);
+        if(label == 1)
+        {
+          askinput(tag);
+          break;
+        }
+        if(label == 2)
+        {
+          lcd.clear();
+          setCursor(0,0);
+          printString("TODO");
+          delay(1000);
+          break;
+        }
+        if(label == 3)
+        {
+          lcd.clear();
+          setCursor(0,0);
+          printString("TODO");
+          delay(1000);
+          break;
+        }
+        if(label == 4)
+        {
+          break;
+        }
+      }
+      tiima = millis();
+    }
+    lcd.clear();
+    setCursor(0,0);
   }
-}
-
-
-
-
-void startSD()
-{
-  digitalWrite(10,HIGH);
-  digitalWrite(A1,LOW);
-  SD.begin(A1);
 }
